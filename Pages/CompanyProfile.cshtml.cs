270548ee0ae096aabefcd78d2902cc391a0f17d6
nwpelletier <124799425+nwpelletier@users.noreply.github.com>
2023-11-21 22:41:44 -0500

    User and Company profile pages
    Blank for now - adding formatting shortly
    Will likely move to "profiles" subfolder
    Will add links on _Layout navbar

---
+++ Pages/CompanyProfile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FSD08_AppDev2Project.Pages
+{
+    public class CompanyProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/UserProfile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FSD08_AppDev2Project.Pages
+{
+    public class UserProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
