4b232354cae64f9cc2f7f9a558e67c6521c6d374
nwpelletier <124799425+nwpelletier@users.noreply.github.com>
2023-11-22 00:28:51 -0500

    User page
    -no styling
    -commented out the ApplicationUser model section
    -using hard coded values just for display purposes

--- Pages/UserProfile.cshtml.cs
+++ Pages/UserProfile.cshtml.cs
@@ -1,12 +1,34 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using FSD08_AppDev2Project.Data;
+using FSD08_AppDev2Project.Models;
 
 namespace FSD08_AppDev2Project.Pages
 {
     public class UserProfileModel : PageModel
     {
-        public void OnGet()
+        private readonly AppDev2DbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserProfileModel(AppDev2DbContext db, UserManager<ApplicationUser> userManager)
         {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            ApplicationUser = await _userManager.GetUserAsync(User);
+
+// TODO: uncomment this once login persists
+            // if (ApplicationUser == null)
+            // {
+            //     return RedirectToPage("Index");
+            // }
+            return Page();
         }
     }
 }
