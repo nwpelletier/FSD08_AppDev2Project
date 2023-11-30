804b4da4f54944de20eb5c948a4f8cc5d0e2132b
nwpelletier <124799425+nwpelletier@users.noreply.github.com>
2023-11-30 10:34:32 -0500

    user table in admin properly displays role
    not using "role" column; used dictionary/hash-map (list roles, find users for that role) to populate column in table

--- Pages/Admin.cshtml.cs
+++ Pages/Admin.cshtml.cs
@@ -1,29 +1,27 @@
-using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 using FSD08_AppDev2Project.Data;
 using FSD08_AppDev2Project.Models;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace FSD08_AppDev2Project.Pages
 {
     public class AdminModel : PageModel
     {
         private readonly AppDev2DbContext _db;
-        public List<ApplicationUser> ApplicationUsers { get; set; }
-        public AdminModel(AppDev2DbContext db)
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AdminModel(AppDev2DbContext db, UserManager<ApplicationUser> userManager)
         {
             _db = db;
+            _userManager = userManager; 
         }
+
+        public List<ApplicationUser> ApplicationUsers { get; set; }
+        public Dictionary<string, List<string>> UserRoles { get; set; }
         public List<Company> Companys { get; set; }
-
         public List<Review> Reviews { get; set; }
-
         public List<Job> Jobs { get; set; }
 
         public void OnGet()
@@ -32,6 +30,13 @@ namespace FSD08_AppDev2Project.Pages
             ApplicationUsers = _db.ApplicationUsers.ToList();
             Reviews = _db.Reviews.ToList();
             Jobs = _db.Jobs.ToList();
+
+            UserRoles = new Dictionary<string, List<string>>();
+            foreach (var user in ApplicationUsers)
+            {
+                var roles = _userManager.GetRolesAsync(user).Result;
+                UserRoles.Add(user.Id, roles.ToList());
+            }
         }
     }
 }
